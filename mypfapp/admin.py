be4ceb4b02cb68fe20481198b58f3f0132a48d9f
jisoleil <janggh1012@naver.com>
2023-05-13 03:10:18 +0900

    templates and url

--- mypfapp/admin.py
+++ mypfapp/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Question
+from .models import Question,Choice
 
-admin.site.register(Question)
+admin.site.register(Question),
+admin.site.register(Choice)
--- mypfapp/views.py
+++ mypfapp/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpRequest, HttpResponse,Http404
+from django.template import loader
+from .models import Question
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the mypfapp index.")
+def index(request ):
+    lastest_question_list=Question.objects.order_by('-pub_date')[:5] #날짜순으로 
+    #output=', '.join([q.question_text for q in lastest_question_list]) #콤마로 연결하겠다.
+    #template=loader.get_template('mypfapp/index.html') 
+    context={
+        'lastest_question_list':lastest_question_list
+    }#연결해준다. 
+    return render(request,'mypfapp/index.html',context)
+
+#404에러 일으키기
+def detail(request,question_id):
+#      try:
+#         question=Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404('Question does not exist') 
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'mypfapp/detail.html',{'question':question})
+
+def results(request,question_id):
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("you're voing on question %s." %question_id)
